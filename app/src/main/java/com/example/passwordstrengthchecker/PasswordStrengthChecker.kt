794a851bf791a849ba10c0bea11e870c7a5488bf
anaghalalu123 <anaghalalu777@gmail.com>
2025-04-05 16:41:35 -0400

    PasswordStrengthChecker

---
+++ app/src/main/java/com/example/passwordstrengthchecker/PasswordStrengthChecker.kt
@@ -0,0 +1,18 @@
+package com.example.passwordstrengthchecker
+
+fun main(){
+    println("Enter you password: ")
+    val passWord = readLine()?:""
+
+    val hasLength = passWord.length>8
+    val hasDigit = passWord.any{it.isDigit()}
+    println("\n Password strength checker")
+    println("Minimum 8 characters: $hasLength")
+    println("Contains atleast 1 digit :$hasDigit")
+
+    if(hasDigit&& hasLength){
+        println("Password is strong")
+    }else{
+        println("Weak password")
+    }
+}
